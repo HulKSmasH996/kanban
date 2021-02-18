6e66a6205726d024a910e4a1eba0549e6a768793
Tarun kumar parashar <57700863+HellRaiderz123@users.noreply.github.com>
2021-02-18 08:14:32 +0530

    jar packing and corc enable in project services

--- src/main/java/com/mytasknowcobackend/kanban/api/ProjectsController.java
+++ src/main/java/com/mytasknowcobackend/kanban/api/ProjectsController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping("api/v1/projects")
 @RestController
 public class ProjectsController {
